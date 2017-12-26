3f1f61e50228ecae6817e1571a9f459820dad8bd
KevinAlexis <chitimi@gmail.com>
2017-12-26 17:28:47 -0600

    Electrocardiogram first commit

--- app/src/main/java/com/ricardo/elias/alexis/myheartcouch/Fragments/FragmentECG.java
+++ app/src/main/java/com/ricardo/elias/alexis/myheartcouch/Fragments/FragmentECG.java
@@ -73,6 +73,7 @@ public class FragmentECG extends Fragment implements View.OnClickListener {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ecg, container, false);
         misPlaying = false;
+
         mDBWaveAdapter = DBWaveAdapter.getInstance(getActivity());
         mValues = new ArrayList<>();
         mFloatingActionButton = (FloatingActionButton) view.findViewById(R.id.button_playbutton);
